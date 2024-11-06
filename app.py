f669a0328de2c667429317d244860ba5f9cc4259
Krasimir <krasi4367@gmail.com>
2024-11-06 23:14:32 +0100

    made some changes

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from flask import render_template, Flask
-import folium
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    austria = [47.5162, 14.5501]
-    map = folium.Map(location=austria,zoom_start= 7)
-    folium.Circle(
-        location=[48.2082, 16.3738],
-        radius=1000,
-        color="blue",
-        fill_color="blue",
-        tooltip="Vienna"
-    ).add_to(map)
-    map_html = map._repr_html_()
-    return render_template("home.html", map_html=map_html)
 
 @app.route('/Login')
 def login():
     return render_template('login.html')
 
-@app.route('/Signup')
+@app.route('/')
 def signup():
     return render_template('signup.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
