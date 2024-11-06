3d860d0df5861c9ee69ac003e83a4b91a8b9cd4e
Krasimir <krasi4367@gmail.com>
2024-11-06 18:39:15 +0100

    map

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import render_template, Flask
+import folium
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    austria = [47.5162, 14.5501]
+    map = folium.Map(location=austria,zoom_start= 7)
+    folium.Circle(
+        location=[48.2082, 16.3738],
+        radius=1000,
+        color="blue",
+        fill_color="blue",
+        tooltip="Vienna"
+    ).add_to(map)
+    map_html = map._repr_html_()
+    return render_template("home.html", map_html=map_html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ security.py
@@ -0,0 +1,3 @@
+import re
+
+pattern = r'^(krasi4367@gmail\.com|[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)?(gov|mil|gouv|gov\.[a-z]{2}|govt|canada\.ca))$'
